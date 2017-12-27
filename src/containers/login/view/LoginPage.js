dec7244e4ccaab30c57806eab04f982d1cd59223
liushenglin <liushenglin@flybycloud.com>
2017-12-27 18:27:07 +0800

    首页修改

--- src/containers/login/view/LoginPage.js
+++ src/containers/login/view/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, WingBlank, Icon, List, Toast } from 'antd-mobile';
+import { Button, WingBlank, Icon, List, InputItem, Toast, TabBar } from 'antd-mobile';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { login } from '../actions';
@@ -14,6 +14,43 @@ const mockUser = {
 }
 
 class LoginPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      fullScreen: false,
+    };
+  }
+
+  renderContent(pageText) {
+    return (
+      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+        <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+        <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({
+              hidden: !this.state.hidden,
+            });
+          }}
+        >
+          Click to show/hide tab-bar
+        </a>
+        <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({
+              fullScreen: !this.state.fullScreen,
+            });
+          }}
+        >
+          Click to switch fullscreen
+        </a>
+      </div>
+    );
+  }
 
   onPressAbout = () => {
     this.props.history.push("/about");
@@ -30,28 +67,119 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <WingBlank className="App">
-        <div className="App-header">
-          <Icon type="success" size="lg" />
-          <h2 className="">Welcome to React</h2>
-        </div>
+      <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
 
-        <ul>
-          <li><Link to="/">首页</Link></li>
-          <li><Link to="/about">关于</Link></li>
-          <li><Link to="/repos">列表</Link></li>
-        </ul>
 
-        <List renderHeader={() => '测试'} className="my-list">
-          <Item arrow="horizontal" extra={'extra content'} onClick={this.onPressAbout}>About</Item>
-          <Item arrow="horizontal" platform="android" onClick={this.onPressRepos}>Repos</Item>
-          <Item arrow="horizontal" multipleLine platform="android" onClick={() => { }}>
-            ListItem<Brief>There may have water ripple effect of <br /> material if you set the click event.</Brief>
-          </Item>
-        </List>
 
-        <Button type="primary" style={{ marginTop: 20 }} onClick={this.onSubmit}>确认</Button>
-      </WingBlank>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="Life"
+            key="Life"
+            icon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat'
+            }}
+            />
+            }
+            selectedIcon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat'
+            }}
+            />
+            }
+            selected={this.state.selectedTab === 'blueTab'}
+            badge={1}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            {this.renderContent('Life')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+              }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+              }}
+              />
+            }
+            title="Koubei"
+            key="Koubei"
+            badge={'new'}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId1"
+          >
+            {this.renderContent('Koubei')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+              }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+              }}
+              />
+            }
+            title="Friend"
+            key="Friend"
+            dot
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+            {this.renderContent('Friend')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            title="My"
+            key="my"
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab',
+              });
+            }}
+          >
+            {this.renderContent('My')}
+          </TabBar.Item>
+        </TabBar>
+      </div>
     );
   }
 }
